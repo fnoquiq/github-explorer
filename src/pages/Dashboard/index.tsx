fe944006dc323b4d5b44edaa282bf63ecdcf9d45
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-07-31 15:37:06 -0300

    :rocket: Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/11557379?s=460&u=91134457a093500f4b1d2da5e0278cda15a4f18e&v=4"
+            alt="Gabriel Mesquita"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/11557379?s=460&u=91134457a093500f4b1d2da5e0278cda15a4f18e&v=4"
+            alt="Gabriel Mesquita"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/11557379?s=460&u=91134457a093500f4b1d2da5e0278cda15a4f18e&v=4"
+            alt="Gabriel Mesquita"
+          />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
